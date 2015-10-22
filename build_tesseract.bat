5a140d46edd32f27c68282b7c11dabf20ebd5787
Tobias Peirick <tobias.peirick@d-velop.de>
2015-10-22 16:36:53 +0200

    Added helper to compile tesseract.exe as standalone commandline

--- build_tesseract.bat
+++ build_tesseract.bat
@@ -1,4 +1,4 @@
 git clone --depth=1 --single-branch git://github.com/tesseract-ocr/tesseract.git  --branch="master" "tesseract_master"
 
 call "%programfiles% (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-msbuild tesseract.sln /property:Configuration=Release /property:Platform=Win32
+msbuild tesseract.sln /p:Configuration=Release /p:Platform=x86 /m
