a95c612fc389d1946c12927471fc3936c8a8df2f
Tobias Peirick <tobias.peirick@d-velop.de>
2016-03-08 16:40:59 +0100

    Enabled __SSE__ for x64 Build Errorhandling in build_tesseract.bat

--- build_tesseract.bat
+++ build_tesseract.bat
@@ -1,3 +1,17 @@
 git clone --depth=1 --single-branch git://github.com/tesseract-ocr/tesseract.git  --branch="master" "tesseract_master"
+
 call "%programfiles% (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+@if %errorlevel% NEQ 0 GOTO ERROR
+
+rmdir /S /Q Binaries
+@if %errorlevel% NEQ 0 GOTO ERROR
+
 msbuild tesseract.sln /p:Configuration=Release /p:Platform=x86 /m
+@if %errorlevel% NEQ 0 GOTO ERROR
+
+@GOTO END
+:ERROR
+@ECHO "Program failed, please check this log file for errors ..." 
+@ECHO Errorlevel: %errorlevel%
+@EXIT /B %errorlevel%
+:END
