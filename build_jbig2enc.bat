615222e2bcb66afbfa4bfda3d6c7fec728b09f9c
Tobias Peirick <peirick@gmail.com>
2017-06-06 12:43:00 +0200

    Build scripts keep track the architectures.

--- build_jbig2enc.bat
+++ build_jbig2enc.bat
@@ -1,2 +1,5 @@
-call "%programfiles% (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+@if %OS%==32BIT call "%programfiles%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+@if %OS%==64BIT call "%programfiles% (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
 msbuild jbig2enc.sln /p:Configuration=Release /p:Platform=x86 /m
--- build_tesseract.bat
+++ build_tesseract.bat
@@ -1,10 +1,12 @@
 git submodule update --init --recursive
 @if %errorlevel% NEQ 0 GOTO ERROR
 
-call "%programfiles% (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+@if %OS%==32BIT call "%programfiles%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+@if %OS%==64BIT call "%programfiles% (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 @if %errorlevel% NEQ 0 GOTO ERROR
 
-rmdir /S /Q Binaries
+rmdir /S /Q Release
 @if %errorlevel% NEQ 0 GOTO ERROR
 
 msbuild tesseract.sln /p:Configuration=Release /p:Platform=x86 /m
